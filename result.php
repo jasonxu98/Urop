73a1c595e9a5e563384d2fc00d6ee56e1d9f248b
jasonxu <jasonxu@umich.edu>
2018-03-23 14:43:42 -0400

    changes

--- result.php
+++ result.php
@@ -1,10 +1,10 @@
 <html>
 <body>
 
-Source: <?php echo $_POST[“src”]; ?><br>
-Destination: <?php echo $_POST[“des”]; ?><br>
-Start time: <?php echo $_POST[“start_time”]; ?><br>
-End time: <?php echo $_POST[“end_time”]; ?><br>
+Source: <?php echo $_POST[‘src’]; ?><br>
+Destination: <?php echo $_POST[‘des’]; ?><br>
+Start time: <?php echo $_POST[‘start_time’]; ?><br>
+End time: <?php echo $_POST[‘end_time’]; ?><br>
 
 </body>
 </html>
