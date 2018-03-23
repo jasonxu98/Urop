36866017238eb6458dd145e59c9318b77420b258
jasonxu <jasonxu@umich.edu>
2018-03-23 14:38:29 -0400

    changes

--- result.php
+++ result.php
@@ -3,8 +3,8 @@
 
 Source: <?php echo $_POST[“src”]; ?><br>
 Destination: <?php echo $_POST[“des”]; ?><br>
-Start time: <?php echo $_POST[“des”]; ?><br>
-End time: <?php echo $_POST[“des”]; ?><br>
+Start time: <?php echo $_POST[“start_time”]; ?><br>
+End time: <?php echo $_POST[“end_time”]; ?><br>
 
 </body>
 </html>
