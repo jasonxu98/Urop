ae0d0c04899984830c97217fe61ed8d5d13abb03
jasonxu <jasonxu@umich.edu>
2018-03-23 14:51:48 -0400

    changes

--- result.php
+++ result.php
@@ -1,10 +1,22 @@
 <html>
+
+
+if ( isset( $_POST[‘src’] ) ) {
+$src = $_POST[‘src’];
+$des = $_POST[‘des’];
+echo $src; 
+echo '<h3>Connected successfully</h3>';
+} 
+
 <body>
 
 Source: <?php echo $_POST[‘src’]; ?><br>
 Destination: <?php echo $_POST[‘des’]; ?><br>
 Start time: <?php echo $_POST[‘start_time’]; ?><br>
 End time: <?php echo $_POST[‘end_time’]; ?><br>
+
+
+
 
 </body>
 </html>
