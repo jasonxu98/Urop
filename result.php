ce2dc3d5b229d418bb8a891512990132156248d2
jasonxu <jasonxu@umich.edu>
2018-03-23 14:17:06 -0400

    modify files

---
+++ result.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Source: <?php echo $_POST[“src”]; ?><br>
+Destination: <?php echo $_POST[“des”]; ?><br>
+Start time: <?php echo $_POST[“des”]; ?><br>
+End time: <?php echo $_POST[“des”]; ?><br>
+
+</body>
+</html>
