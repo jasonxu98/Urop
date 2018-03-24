e0fee6b791b614d4692f3b3ae1d7cc50d3a69597
jasonxu <jasonxu@umich.edu>
2018-03-23 20:22:08 -0400

    changes

--- result.php
+++ result.php
@@ -10,7 +10,7 @@
 <body>
 <?php
 
-$src = $des = $start_time = $end_time = “”;
+$src = $des = $start_time = $end_time = "";
 if (isset( $_POST["src"] )) {
 $src = $_POST["src"];
 $des = $_POST["des"];
@@ -23,8 +23,8 @@ echo '<h3>Connected successfully</h3>';
 <h1> Your input </h1>
 Source: <?php echo $_POST["src"]; ?><br>
 Destination: <?php echo $_POST["des"]; ?><br>
-Start time: <?php echo $_POST[""start_time""]; ?><br>
-End time: <?php echo $_POST[""end_time""]; ?><br><br>
+Start time: <?php echo $_POST["start_time"]; ?><br>
+End time: <?php echo $_POST["end_time"]; ?><br><br>
 
 
 
