c83eb8f12d01aec51eae1370e5fc22f5f665737f
jasonxu <jasonxu@umich.edu>
2018-03-23 20:36:03 -0400

    changes

--- result.php
+++ result.php
@@ -12,8 +12,8 @@
 <h1> Your input </h1>
 Source: <?php echo $_POST["src"]; ?><br>
 Destination: <?php echo $_POST["des"]; ?><br>
-Start time: <?php echo $_POST['start_time']; ?><br>
-End time: <?php echo $_POST['end_time']; ?><br><br>
+Start time: <?php echo htmlspecialchars($_POST['start_time’]); ?><br>
+End time: <?php echo htmlspecialchars($_POST['end_time’]); ?><br><br>
 
 
 
