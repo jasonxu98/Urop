b27ae1498c195f40ecb248fa14e13b658a6cc939
jasonxu <jasonxu@umich.edu>
2018-03-23 20:44:56 -0400

    changes

--- result.php
+++ result.php
@@ -9,10 +9,13 @@
 
 <body>
 
+<?php
 <h1> Your input </h1>
 $src = $des = $start_time = $end_time = "";
 $src=$_POST["src"]
-Source: <?php echo $src; ?><br>
+echo "Source: “;<br>
+echo $src;<br>
+?>
 Destination: <?php echo $_POST["des"]; ?><br>
 Start time: <?php echo htmlspecialchars($_POST["start_time"]); ?><br>
 End time: <?php echo htmlspecialchars($_POST["end_time"]); ?><br><br>
