ee6137939cb772d3b96b04f35a46e50b268a9fad
jasonxu <jasonxu@umich.edu>
2018-03-24 02:23:05 -0400

    changes

--- result.php
+++ result.php
@@ -15,13 +15,26 @@ Destination: <?php echo $_POST["des"]; ?><br>
 Start time: <?php echo $_POST["start_time"]; ?><br>
 End time: <?php echo $_POST["end_time"]; ?><br><br>
 
+<h2> The output</h2><br>
 
 <?php 
 $src = $_POST["src"];
 $des = $_POST["des"];
 $start_time = $_POST["start_time"];
 $end_time = $_POST["end_time"];
-echo $src;
+
+$host        = "host = localhost";
+$port        = "port = 5432";
+$dbname      = "dbname = jasonxu";
+$credentials = "user = jasonxu password=xzk3136";
+
+$db = pg_connect( "$host $port $dbname $credentials"  );
+if(!$db) {
+    echo "Error : Unable to open database\n";
+} else {
+      echo "Opened database successfully\n";
+}
+
 ?>
 
 
