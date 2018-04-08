fb22af521df18e5688f1c04c1d26651a1b846f33
jasonxu <jasonxu@umich.edu>
2018-04-08 13:33:15 -0400

    changes

--- result.php
+++ result.php
@@ -8,10 +8,18 @@
 </head>
 
 <body>
+<?php
+    $src = $_POST["src"];
+    $des = $_POST["des"];
+    $start_time = $_POST["start_time"];
+    $end_time = $_POST["end_time"];
+    $epoch_start = strtotime($start_time);
+    $epoch_end = strtotime($end_time);
+    ?>
 
 <h1> Your input </h1>
-Source: <?php echo $_POST["src"]; ?><br>
-Destination: <?php echo $_POST["des"]; ?><br>
+Source: <?php echo $src; ?><br>
+Destination: <?php echo $des; ?><br>
 Start time: <?php echo $_POST["start_time"]; ?>(epoch:<?php echo strtotime($_POST["start_time"]); ?>)<br>
 End time: <?php echo $_POST["end_time"]; ?>(epoch:<?php echo strtotime($_POST["end_time"]); ?>)<br><br>
 
@@ -19,14 +27,6 @@ End time: <?php echo $_POST["end_time"]; ?>(epoch:<?php echo strtotime($_POST["e
 <h2> The output</h2><br>
 
 
-<?php 
-$src = $_POST["src"];
-$des = $_POST["des"];
-$start_time = $_POST["start_time"]; 
-$end_time = $_POST["end_time"];
-$epoch_start = strtotime($start_time);
-$epoch_end = strtotime($end_time);
-?>
 
 
 <?php
