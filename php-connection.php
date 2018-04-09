19a85c940ebeaac93819ee8f825ce23b3235b117
jasonxu <jasonxu@umich.edu>
2018-04-09 16:21:27 -0400

    changes

--- php-connection.php
+++ php-connection.php
@@ -20,7 +20,7 @@ try {
 }
 
 # Create query of table 
-  $sql_query="select domain||'(ipv4)' from serverlookupbackup where ipv4 S NOT NULL";
+  $sql_query="select domain||'(ipv4)' as domain from serverlookupbackup where ipv4 IS NOT NULL";
   try {
   $list = $dbh->query($sql_query);
   if ($list === false){
