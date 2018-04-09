66c70380d51d8215b47be6ae5180ea6485424b3a
jasonxu <jasonxu@umich.edu>
2018-04-09 16:45:20 -0400

    changes

--- php-connection.php
+++ php-connection.php
@@ -21,20 +21,23 @@ try {
 
 # Create query of table 
     # $sql_query="select domain||'(ipv4)' as domain from serverlookupbackup where ipv4 IS NOT NULL";
-    $sql_query="select ipv4 from serverlookupbackup";
+    #$sql_query="select ipv4 from serverlookupbackup";
+    $sql_query="select * from serverlookupbackup";
 
   try {
   $list = $dbh->query($sql_query);
+      
+      $arr = pg_fetch_all($list);
+      
+      print_r($arr);
+
   if ($list === false){
 	die("Error executing the query: $sql_query");
   }
 } catch (PDOExeption $e) {
   echo $e->getMessage();
 }
-    $arr = pg_fetch_all($list);
     
-    print_r($arr);
-
 
     while($row_list=pg_fetch_result($list, 1,0)){
         echo "WEIRD<br>";
