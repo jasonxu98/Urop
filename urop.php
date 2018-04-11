d34abe7460f3f13189e4095ff0d84ab4de7b190f
jasonxu <jasonxu@umich.edu>
2018-04-11 16:59:48 -0400

    Changes

--- urop.php
+++ urop.php
@@ -191,17 +191,22 @@ function limitDes(str) {
         var lastFour = str.substr(str.length - 4);
         var check = '';
     
-    
-    
-    if (lastFour == 'ipv4') {
-
+    if (lastFour == 'ipv6') {
+        document.getElementById("text").innerHTML= 'ipv6';
+    } else if (lastFour == 'ipv4') {
+        <?php
+        $sql_query_three="select domain||'(ipv4)' as domain from serverlookupbackup where ipv6 IS NULL";
+        $list_three = $dbh->query($sql_query_three);
+        
+        while($row_list_three = $list_three->fetch(PDO::FETCH_ASSOC)):
+            $to_add = $row_list_three["domain"];
+        ?>
+        ip_address = "<?php echo $to_add; ?>";
+        check = check + ip_address;
+        str1 = str1 + '<option value = "'  + ip_address '"></option>';
+        <?php endwhile; ?>
         document.getElementById("text").innerHTML= lastFour;
-    } else if (lastFour == 'ipv6') {
-        document.getElementById("text").innerHTML= 'not valid';
     }
-    
-    
-    
     
 }
 </script>
