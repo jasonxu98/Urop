c79c2eb008de154edc4e689445457bdee4f8148a
jasonxu <jasonxu@umich.edu>
2018-04-11 17:32:31 -0400

    Changes

--- urop.php
+++ urop.php
@@ -193,14 +193,29 @@ document.getElementById("demo").innerHTML = Date();
 
 
 function limitDes(str) {
-        var str1 = '<option value = "jason"></option>';
+        var str1 = '';
+        var str2 = '';
         var ip_address = 'data:';
+    var ip_address_ = '';
         str = str.slice(0,-1);
         var lastFour = str.substr(str.length - 4);
         var check = '';
     
     if (lastFour == 'ipv6') {
-        document.getElementById("text").innerHTML= 'ipv6';
+        <?php
+        include 'dtb.php';
+        $sql_query_four="select domain||'(ipv6)' as domain from serverlookupbackup where ipv6 IS NULL";
+        $list_four = $dbh->query($sql_query_four);
+        
+        while($row_list_four = $list_four->fetch(PDO::FETCH_ASSOC)):
+            $to_add_ = $row_list_four["domain"];
+        ?>
+        ip_address_ = "<?php echo $to_add_; ?>";
+        str2 = str2 + '<option value = "'  + ip_address_ + '"></option>';
+        <?php endwhile; ?>
+        document.getElementById("text").innerHTML= lastFour;
+        document.getElementById("browser").innerHTML= str2;
+
     } else if (lastFour == 'ipv4') {
         <?php
         include 'dtb.php';
