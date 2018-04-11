2c4d3d8f888865975ff8ee0702a7723d0971772b
jasonxu <jasonxu@umich.edu>
2018-04-11 16:18:04 -0400

    Changes

--- urop.php
+++ urop.php
@@ -175,13 +175,13 @@ while($row_list_two = $list_two->fetch(PDO::FETCH_ASSOC)):
     ?>
 </p>
 
-<?php
+
 <script>
 function updateClock() {
 document.getElementById("demo").innerHTML = Date();
 }
 </script>
-?>
+
 
 
 <script>
@@ -203,7 +203,7 @@ function limitDes(str) {
             
                 while($row_list_three = $list_three->fetch(PDO::FETCH_ASSOC)):
                 ?>
-            
+            ip_address = '<?php echo row_list_three["domain"]; ?>';
             str1 = str1 + '<option value = "'  + '"></option>';
             <?php endwhile; ?>
             document.getElementById("text").innerHTML= ip_address;
