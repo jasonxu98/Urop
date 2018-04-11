28ddd475dec508f9bcaf5b9ef90ef79e37bdf269
jasonxu <jasonxu@umich.edu>
2018-04-11 16:02:55 -0400

    Changes

--- urop.php
+++ urop.php
@@ -201,7 +201,7 @@ function limitDes(str) {
             
                 while($row_list_three = $list_three->fetch(PDO::FETCH_ASSOC)):
                 ?>
-            ip_address = <?php echo "kjfngjkasg"; ?>
+            
             str1 = str1 + '<option value = "'  + '"></option>';
             <?php endwhile; ?>
             document.getElementById("text").innerHTML= ip_address;
