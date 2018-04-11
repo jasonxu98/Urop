b724ca484d058af431d5cddda0d7e165871c0c65
jasonxu <jasonxu@umich.edu>
2018-04-11 15:39:16 -0400

    Changes

--- urop.php
+++ urop.php
@@ -199,10 +199,10 @@ function limitDes(str) {
                 $sql_query_three="select domain||'(ipv4)' as domain from serverlookupbackup where ipv6 IS NULL";
                 $list_three = $dbh->query($sql_query_three) or die('error');
             
-                while($row_list_two = $list_three->fetch(PDO::FETCH_ASSOC)):
+                while($row_list_three = $list_three->fetch(PDO::FETCH_ASSOC)):
                 ?>
-                                    
-                    str1 = str1 + '<option value = "' + <?php echo row_list_two["domain"]; ?> + '"></option>';
+                    
+                    str1 = str1 + '<option value = "' + <?php echo row_list_three["domain"]; ?> + '"></option>';
 
                     
             
