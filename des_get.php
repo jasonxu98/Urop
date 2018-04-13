44d4dd517e6c0f1df6a251d43edbf833835d26ff
jasonxu <jasonxu@umich.edu>
2018-04-13 16:19:35 -0400

    changes

--- des_get.php
+++ des_get.php
@@ -7,10 +7,6 @@
     while($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC)):
     $six = $row_list_domain["ipv6"];
     endwhile;
-    
-    $list_four = $dbh->query("select dest as dest from traceroute where src = '" . $six . "';")
-    while($row_list_src = $list_four->fetch(PDO::FETCH_ASSOC)):
-    $dest = $row_list_src["dest"];
-    echo $dest;
-    endwhile;
+    echo $six;
+   
 ?>
