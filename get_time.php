b94c75b7b3c26e440bd3d8c0f132fc92a4e25d59
jasonxu <jasonxu@umich.edu>
2018-04-13 18:36:43 -0400

    changes

--- get_time.php
+++ get_time.php
@@ -3,8 +3,21 @@
     $ip1 = $_POST["ip1"];
     $dest = $_POST["dest"];
     $ip2 = $_POST["ip2"];
-    echo $src;
-    echo $ip1;
-    echo $dest;
-    echo $ip2;
-?>
+    include 'dtb.php';
+    $sql_query_domain1="select " . $ip1 .  " as " . $ip1 . " from serverlookupbackup where domain = '" . $src . "';";
+    $sql_query_domain2="select " . $ip2 .  " as " . $ip2 . " from serverlookupbackup where domain = '" . $dest . "';";
+    $list1 = $dbh->query($sql_query_domain1);
+    $list2_three = $dbh->query($sql_query_domain2);
+    
+    while($row1 = $list1->fetch(PDO::FETCH_ASSOC)):
+    $ipaddress1 = $row1[$ip1];
+    endwhile;
+    
+    while($row2 = $list2->fetch(PDO::FETCH_ASSOC)):
+    $ipaddress2 = $row2[$ip2];
+    endwhile;
+    
+    echo ipaddress1;
+    echo ipaddress2;
+    
+   ?>
