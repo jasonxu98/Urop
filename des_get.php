6dbecae6a1b7e394636daa7ccc8f386a1e81f72d
jasonxu <jasonxu@umich.edu>
2018-04-13 15:59:07 -0400

    changes

--- des_get.php
+++ des_get.php
@@ -1,4 +1,16 @@
 <?php
-    $des = $_POST["src"];
-    echo $des;
+    $src = $_POST["src"];
+
+    
+    
+    
+    include 'dtb.php';
+    $sql_query_domain="select ipv6 as ipv6 from serverlookupbackup where domain = '" . $src . "';";
+    $list_three = $dbh->query($sql_query_domain);
+    
+    while($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC)):
+    $six = $row_list_domain["ipv6"];
+    echo $six;
+    endwhile;
+    ?>
 ?>
