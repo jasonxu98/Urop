622d55814071c1f7883c4639659da05f38eb9b59
jasonxu <jasonxu@umich.edu>
2018-04-13 18:57:33 -0400

    changes

--- get_time.php
+++ get_time.php
@@ -18,14 +18,14 @@
     endwhile;
     
     
-    $sql_query_time = "select src, dest, to_char(min(timestamp), 'YYYY-MM-DD\"T\"HH24:MI:SS') as MinTime, to_char(max(timestamp), 'YYYY-MM-DD\"T\"HH24:MI:SS') as MaxTime from rawtracedatabackup where src = '" . $ipaddress1 . "' and dest = '" . $ipaddress2 . "' group by src, dest;";
+    $sql_query_time = "select src, dest, to_char(min(timestamp), 'YYYY-MM-DD\"T\"HH24:MI:SS') as MinTime, to_char(max(timestamp), 'YYYY-MM-DD\"T\"HH24:MI:SS') as MaxTime from rawtracedatabackup where src = '" . $ipaddress1 . "' and dest = '" . $ipaddress2 . "' group by src, dest,MinTime,MaxTime;";
     $list3 = $dbh->query($sql_query_time);
     if ($list3 === false) {
         echo notfound;
     } else {
     while($row3 = $list3->fetch(PDO::FETCH_ASSOC)):
-    $min = $row3["src"];
-    $max = $row3["dest"];
+    $min = $row3["MinTime"];
+    $max = $row3["MaxTime"];
     endwhile;
     echo $min;
     echo $max;
