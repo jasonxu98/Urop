0b54c360d3f60fe02df326920941cc4369c0dffb
jasonxu <jasonxu@umich.edu>
2018-04-13 18:44:49 -0400

    changes

--- get_time.php
+++ get_time.php
@@ -20,4 +20,15 @@
     echo $ipaddress1;
     echo $ipaddress2;
     
+    $sql_query_time = "select src, dest, to_char(min(timestamp), 'YYYY-MM-DD"T"HH24:MI:SS') as MinTime, to_char(max(timestamp), 'YYYY-MM-DD"T"HH24:MI:SS') as MaxTime from rawtracedatabackup where src = '" . $ipaddress1 . "' and dest = '" . $ipaddress2 . "' group by src, dest;";
+    $list3 = $dbh->query($sql_query_time);
+    while($row3 = $list3->fetch(PDO::FETCH_ASSOC)):
+    $min = $row3["MinTime"];
+    $max = $row3["MaxTime"];
+    endwhile;
+    echo $min;
+    echo $max;
+
+
+    
    ?>
