1db7b0f7c0e5feb41bc6658a89c814199c0accfc
jasonxu <jasonxu@umich.edu>
2018-04-16 11:10:23 -0400

    chang

--- result.php
+++ result.php
@@ -15,13 +15,14 @@
     $end_time = $_POST["end_time"];
     $epoch_start = strtotime($start_time);
     $epoch_end = strtotime($end_time);
+    ?>
     <h1> Your input </h1>
 Source: <?php echo $src; ?><br>
 Destination: <?php echo $des; ?><br>
 Start time: <?php echo $_POST["start_time"]; ?>(epoch:<?php echo strtotime($_POST["start_time"]); ?>)<br>
 End time: <?php echo $_POST["end_time"]; ?>(epoch:<?php echo strtotime($_POST["end_time"]); ?>)<br><br>
 
-?>
+
 <h2> The output</h2><br>
 
 
