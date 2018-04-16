c828dd38c83d3f5f622e939977523148ab6ae6e6
jasonxu <jasonxu@umich.edu>
2018-04-16 11:09:21 -0400

    chang

--- result.php
+++ result.php
@@ -21,12 +21,42 @@ Destination: <?php echo $des; ?><br>
 Start time: <?php echo $_POST["start_time"]; ?>(epoch:<?php echo strtotime($_POST["start_time"]); ?>)<br>
 End time: <?php echo $_POST["end_time"]; ?>(epoch:<?php echo strtotime($_POST["end_time"]); ?>)<br><br>
 
-
+?>
 <h2> The output</h2><br>
 
 
 
 
+<?php
+
+echo "Hello<br>";
+$host        = "t3pers13.physics.lsa.umich.edu";
+#$host        = "localhost";
+$port        = "5432";
+$dbname      = "jasonxu";
+$user = "jasonxu"; 
+$password = "xzk3136";
+
+try {
+  $dbh = new PDO( "pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+  if ($dbh) {
+	echo "Connected to DB $dbname<br>";
+  }
+} catch ( PDOException $e) {
+	echo "Error connecting to DB:".$e->getMessage();
+}
+
+# Create query of table 
+try {
+  $stmt = $dbh->query($sql_query);
+  if ($stmt === false){
+	die("Error executing the query: $sql_query");
+  }
+} catch (PDOExeption $e) {
+  echo $e->getMessage();
+}
+
+?> 
 
 </body>
 </html>
