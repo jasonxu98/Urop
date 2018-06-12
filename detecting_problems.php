b5f92d875965281883b23812b8a87f3c11a9d1d7
jasonxu <jasonxu@umich.edu>
2018-06-12 14:54:30 -0400

    Add a test

--- detecting_problems.php
+++ detecting_problems.php
@@ -6,17 +6,18 @@
     $user = "postgres";
     $password = "xzk3136";
     $dbh1 = new PDO( "pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    
     $sql_query_one="select domain as domain from serverlookupbackup where ipv4 IS NOT NULL";
     $sql_query_two="select domain as domain from serverlookupbackup where ipv6 IS NOT NULL";
     $list = $dbh1->query($sql_query_one) or die('error');
     $list_two = $dbh1->query($sql_query_two) or die('error');
     while($row_list = $list->fetch(PDO::FETCH_ASSOC)):
-    $src = $row_list;
-    $sql_query_domain = "select ipv4 as ipv4 from serverlookupbackup where domain = '" . $src . "';";
-    $list_three = $dbh->query($sql_query_domain);
-    if(!($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC))) {
-        echo "No destination found for : " . $src . "\n";
-    }
+        $src = $row_list;
+        $sql_query_domain = "select ipv4 as ipv4 from serverlookupbackup where domain = '" . $src . "';";
+        $list_three = $dbh1->query($sql_query_domain);
+        if(!($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC))) {
+            echo "No destination found for : " . $src . "\n";
+        }
     endwhile;
     
-    ?>
+?>
