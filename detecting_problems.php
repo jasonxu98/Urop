59ade94cd1f6aecb9815f4b45d8aaca1e80c3378
jasonxu <jasonxu@umich.edu>
2018-06-12 15:12:06 -0400

    Add a test

--- detecting_problems.php
+++ detecting_problems.php
@@ -20,9 +20,9 @@
         endwhile;
         $sql_query_dest = "select DISTINCT dest as dest from traceroutebackup where src = '" . $six . "';";
         $list_four = $dbh1->query($sql_query_dest);
-        while($row_list_dest = $list_four->fetch(PDO::FETCH_ASSOC)):
+        if(!($row_list_dest = $list_four->fetch(PDO::FETCH_ASSOC))) {
             echo "No destination found for : " . $src . "<br>";
-        endwhile;
+        }
     endwhile;
     
 ?>
