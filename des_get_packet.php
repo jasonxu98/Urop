a12d81a6afac759522c82323f8a3679865045dff
System Administrator <root@wlan-141-23-195-129.tubit.tu-berlin.de>
2018-08-01 09:22:00 -0400

    Add things

--- des_get_packet.php
+++ des_get_packet.php
@@ -3,7 +3,7 @@
     include 'dtb.php';
     $sql_query_domain="select distinct dest as dest from rawpacketdata where src = '" . $src . "';";
     $list_three = $dbh->query($sql_query_domain);
-    
+    echo "<option value = \"198.124.252.101\"></option>";
     while($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC)):
         echo "<option value = \"" . $row_list_domain["dest"] . "\"></option>";
     endwhile;
