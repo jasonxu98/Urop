6e3ca04bbe0e595793115a91112f798da8c52e35
System Administrator <root@wlan-141-23-195-129.tubit.tu-berlin.de>
2018-08-01 09:20:08 -0400

    Add things

--- des_get_packet.php
+++ des_get_packet.php
@@ -5,21 +5,7 @@
     $list_three = $dbh->query($sql_query_domain);
     
     while($row_list_domain = $list_three->fetch(PDO::FETCH_ASSOC)):
-    $six = $row_list_domain[$dest];
-    endwhile;
-    
-   
-    #if(pg_num_rows($list_four) == 0) {
-    #   echo "<option value = \"No destinations found for this given source\"></option>";
-    #}
-    
-    echo "<option value = \"" . pg_num_rows($six) . "\"></option>";
-    
-    while($row_list_dest = $six->fetch(PDO::FETCH_ASSOC)):
-        
-    
-    $option = "<option value = \"" . $row_list_dest . "\"></option>";
-    echo $option;
+        echo "<option value = \"" . $row_list_domain["dest"] . "\"></option>";
     endwhile;
    
 ?>
